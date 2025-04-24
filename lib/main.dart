ba7517e9cc3fd7d6ecff2ce935f53450dc6a4c11
mohammad95700 <mohammad.afshar95700@gmail.com>
2025-04-24 23:59:14 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Flutter'),
+          title: const Text('Hello Flutter'),
         ),
-        body: Center(
-          child: Text('سلام دنیا!'),
+        body: const Center(
+          child: Text('Welcome to Flutter!'),
         ),
       ),
     );
